cf3ae5bb710595aa69c948b5ddc77a965f69531c
azmick <azmi.karakaya@ogr.sakarya.edu.tr>
2024-03-16 14:17:26 +0300

    testing the server and config package.json file.

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { chats } = require("./data/data.js");
+const dotenv = require("dotenv");
+
+const app = express();
+
+//.env'yi ayarlamak için şart
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("API Başarılı Bir Şekilde Çalışıyor.");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //chat ekranını yakalıyoruz.
+  const singleChat = chats.find((c) => c._id === req.params.id); //request'imiz
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Sunucu ${PORT} portunda çalışıyor.`));
