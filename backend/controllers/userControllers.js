d9c6415f13aa31b7755d68a019b3eee4d73acf58
azmick <azmi.karakaya@ogr.sakarya.edu.tr>
2024-04-15 09:02:13 +0300

    password encryption with bcryptjs

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -41,7 +41,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
-  if (user && user.password === password) {
+  if (user && (await undefinedser.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
@@ -55,4 +55,4 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = registerUser;
+module.exports = { registerUser, authUser };
